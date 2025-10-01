ec9f93848fb1258fd348fc62349f41c00ac8e37c
felix-proyects <frasesbebor@gmail.com>
2025-10-01 07:41:16 -0400

    Actualizar group-tagall.jsñ

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,18 +1,14 @@
-const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-const pesan = args.join` `
-const oi = `*» INFO :* ${pesan}`
-let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname}≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`
-for (const mem of participants) {
-teks += `┊ꕥ @${mem.id.split('@')[0]}\n`
-}
-teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄⸼`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) })
-}
-
-handler.help = ['todos']
-handler.tags = ['group']
-handler.command = ['todos', 'invocar', 'tagall']
-handler.admin = true
-handler.group = true
-
-export default handler
+const handler = async (m, { conn }) => {
+    const user = global.db.data.users[m.sender];
+        conn.sendMessage(m.chat, {text: `*@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
+      global.db.data.users[m.sender].money = Infinity;
+    global.db.data.users[m.sender].cookies = Infinity;
+  global.db.data.users[m.sender].level = Infinity;
+ global.db.data.users[m.sender].exp = Infinity;
+};
+handler.help = ['cheat'];
+handler.tags = ['owner'];
+handler.command = ['ilimitado', 'infiniy', 'chetar'];
+handler.rowner = true;
+handler.fail = null;
+export default handler;
